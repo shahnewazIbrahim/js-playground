ff609b6e867d2a791e455b92271fb5fdc42c056c
Mamba-dot <shahnewaz886@gmail.com>
2021-12-17 22:39:39 +0600

    new date

---
+++ time.js
@@ -0,0 +1,38 @@
+let today = new Date();
+
+let day = today.getDay();
+
+let dayList = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+
+console.log(`Today is: ${dayList[day].charAt(0).toUpperCase()}${ dayList[day].slice(1)}`);
+
+let hour = today.getHours();
+
+let minute = today.getMinutes();
+
+let second = today.getSeconds();
+
+let prepand = (hour >= 12)? " PM ":" AM ";
+  
+hour = (hour >= 12)? hour - 12: hour;
+
+if(hour === 0 && prepand === 'PM') {
+    if (minute===0 && second===0) { 
+    hour=12;
+    prepand=' Noon';
+  }else { 
+    hour=12;
+    prepand=' PM';
+  }
+}
+
+  if (hour===0 && prepand===' AM ') { 
+    if (minute===0 && second===0) { 
+      hour=12;
+      prepand=' Midnight';
+    }else { 
+      hour=12;
+      prepand=' AM';
+    } 
+  } 
+console.log(`Current Time : ${hour} ${ prepand}:${minute}:${second}`);
