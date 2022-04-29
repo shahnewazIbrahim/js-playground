3c295cbae4d817397a6d6e1c06b1ca8367c3643c
Mamba-dot <shahnewaz886@gmail.com>
2022-04-29 12:58:35 +0600

    done

--- rotation.js
+++ rotation.js
@@ -6,3 +6,6 @@ setInterval(function ()
  text = text[text.length - 1] + text.substring(0, text.length - 1);
   console.log(text);
 }, 100);
+
+let file = 'embroad.txt'
+let extension = file.split('.').pop();
