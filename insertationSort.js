10798ad838a90962f3d985c514a619a3d0633a42
Mamba-dot <shahnewaz886@gmail.com>
2022-05-02 12:42:56 +0600

    insertation sort comment

--- insertationSort.js
+++ insertationSort.js
@@ -4,9 +4,9 @@ let n = arr.length;
 
     for(let i = 1; i < n; i++) {
         // choosing the first element in our unsorted subarray
-        let current = arr[i];
+        let current = arr[i]; //5
         // the last element of our sorted subarray
-        let j = i - 1;
+        let j = i - 1; //0 => 3
 
         while((j > -1) && (current < arr[j])) {
             arr[j + 1] = arr[j];
